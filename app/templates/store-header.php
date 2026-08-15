c57fc74d706e58c09dd636bed0cec792b451e55a
mahamadoukdiarra <mahamadoukdiarra04@gmail.com>
2026-08-15 21:11:15 +0000

    Add Meta Pixel to public storefront

--- app/templates/store-header.php
+++ app/templates/store-header.php
@@ -11,8 +11,27 @@
   <meta name="theme-color" content="#11100f">
   <link rel="stylesheet" href="<?= e(url('/assets/css/app.css')) ?>">
   <link rel="stylesheet" href="<?= e(url('/assets/css/store-responsive.css')) ?>">
+  <!-- Meta Pixel Code -->
+  <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1719750622625367');
+    fbq('track', 'PageView');
+  </script>
+  <!-- End Meta Pixel Code -->
 </head>
 <body>
+  <noscript>
+    <img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=1719750622625367&ev=PageView&noscript=1"
+      alt="">
+  </noscript>
   <div class="notice">Livraison offerte à Bamako · Paiement à la réception</div>
   <header class="store-header">
     <a class="brand" href="<?= e(url('/')) ?>">L’HORLOGER</a>
