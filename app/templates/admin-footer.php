4a18b259224e8f26b45e2f23ba617d4dbe5f23a7
mahamadoukdiarra <mahamadoukdiarra04@gmail.com>
2026-08-29 18:51:48 +0000

    Force le rafraichissement des styles admin

--- app/templates/admin-footer.php
+++ app/templates/admin-footer.php
@@ -1,1 +1,2 @@
-</main></div><link rel="stylesheet" href="<?= e(url('/assets/css/admin-overrides.css')) ?>"></body></html>
+<?php $adminOverridesVersion = (string) (@filemtime(dirname(APP_ROOT) . '/public/assets/css/admin-overrides.css') ?: '1'); ?>
+</main></div><link rel="stylesheet" href="<?= e(url('/assets/css/admin-overrides.css?v=' . $adminOverridesVersion)) ?>"></body></html>
